cea811a2944127732e780e2e225f44921b36fddc
guy <barak4@post.bgu.ac.il>
2024-06-21 13:13:54 +0300

    c

--- src/main/java/Application/ServiceLayer/GameService.java
+++ src/main/java/Application/ServiceLayer/GameService.java
@@ -107,7 +107,7 @@ public class GameService {
             List<UUID> startingPositionsUuid = new LinkedList<>();
             for (Object o : startingPositions){
                 String s = (String) o;
-                if (s.isEmpty() || s == null || s == "" || s.isBlank())
+                if (s == null || s.length() == 0)
                     continue;
                 UUID id = UUID.fromString(s);
                 startingPositionsUuid.add(id);
