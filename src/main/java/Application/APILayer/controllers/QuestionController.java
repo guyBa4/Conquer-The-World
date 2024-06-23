4c497801f9997aac0372e3d9e9735a01531e14cc
guy <barak4@post.bgu.ac.il>
2024-06-23 17:07:11 +0300

    new questions

--- src/main/java/Application/APILayer/controllers/QuestionController.java
+++ src/main/java/Application/APILayer/controllers/QuestionController.java
@@ -50,6 +50,20 @@ public class QuestionController {
         }
     }
 
+    @PostMapping(path = "/add_questions")
+    @ResponseBody
+    public Response<Question> addQuestion(@RequestBody() List<NewQuestion> newQuestions, @RequestHeader(name = HttpHeaders.AUTHORIZATION) String authorizationHeader) {
+        try {
+            tokenHandler.verifyAnyToken(authorizationHeader);
+            if (newQuestions != null)
+                for (NewQuestion newQuestion : newQuestions)
+                    return questionService.addQuestion(newQuestion);
+            return Response.fail(400, "Invalid request");
+        } catch (JSONException e) {
+            return Response.fail(500, "Internal Server Error"); // Internal Server Error
+        }
+    }
+
     @PostMapping(path = "/add_questionnaire")
     @ResponseBody
     public Response<Questionnaire> addQuestionnaire(@RequestBody String inputJson, @RequestHeader(name = HttpHeaders.AUTHORIZATION) String authorizationHeader) {
